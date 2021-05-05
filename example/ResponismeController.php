3a05eeb3c0280cc0fcd32f5f3f974763ae009bbd
Shofa <sofamh@gabi.id>
2021-05-05 11:35:17 +0700

    Example code

---
+++ example/ResponismeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Att\Responisme\Exceptions\StarterKitException;
+
+class ResponismeController extends Controller
+{
+    public function exception()
+    {
+        throw new StarterKitException('Error Exception');
+    }
+
+    public function exceptionWitData()
+    {
+        try {
+            // str_replace();            
+            \DB::table('random')->get();            
+        } catch (\Throwable $th) {
+            // throw (new StarterKitException('Error Exception'))->withData(['a' => 1, 'b' => 2]);
+            // throw (new StarterKitException('Error Exception'))->withData($th)->withCode('02');            
+            throw (new StarterKitException(transMessageException($th)))->withData($th)->withCode('02');
+        }
+    }
+
+    public function responseSuccess()
+    {
+        return responisme()->makeSuccess('resp success', ['value' => true]);
+
+        // return responisme()->withMessage('resp success')
+        //     ->withData(['value' => true])
+        //     ->withHttpCode(201)
+        //     ->build();
+    }
+
+    public function responseError()
+    {
+        return responisme()->makeError('resp error', ['value' => false],'02');
+
+        // return responisme()->withMessage('resp success')
+        //     ->withData(['value' => true])
+        //     ->withErrorCode('03')
+        //     ->withHttpCode(401)
+        //     ->build(false);
+    }
+}
