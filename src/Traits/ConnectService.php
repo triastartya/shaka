ffbffac861fdcdc2d144df55ea1e9257d277373c
Shofa <sofamh@gabi.id>
2021-07-05 16:39:32 +0700

    Trait to call service

---
+++ src/Traits/ConnectService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Att\Responisme\Traits;
+
+use Att\Responisme\Exceptions\StarterKitException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+/**
+ * Configuration response for validate requests
+ */
+trait ConnectService
+{
+    public function callService($url, $method = null, $data = null)
+    {
+        if (empty($url)) {
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Define endpoint of service'
+            ]);
+        }
+
+        $http = Http::acceptJson()
+            ->withHeaders(['Accept' => 'application/json', 'Authorization' => request()->header('authorization')]);
+
+        if ($method === null || $method === 'GET') {
+            return $http->get($url, array_merge($data ?? [], ['auth_user_kong' => request('auth_user_kong')]))->json();                        
+        }elseif( $method === 'POST'){
+            return $http->post($url, array_merge($data ?? [], ['auth_user_kong' => request('auth_user_kong')]))->json();  
+        }elseif( $method === 'PUT'){
+            return $http->put($url, array_merge($data ?? [], ['auth_user_kong' => request('auth_user_kong')]))->json();  
+        }elseif( $method === 'PATCH'){
+            return $http->patch($url, array_merge($data ?? [], ['auth_user_kong' => request('auth_user_kong')]))->json();  
+        }elseif( $method === "DELETE"){
+            return $http->delete($url, array_merge($data ?? [], ['auth_user_kong' => request('auth_user_kong')]))->json();  
+        }
+    }
+
+}
