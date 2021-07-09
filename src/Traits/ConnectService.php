d053a556f61cb3f13fad9dca742a66eb57817acb
Shofa <sofamh@gabi.id>
2021-07-09 14:32:40 +0700

    handling service timeout

--- src/Traits/ConnectService.php
+++ src/Traits/ConnectService.php
@@ -14,41 +14,71 @@ use function PHPUnit\Framework\isNull;
  */
 trait ConnectService
 {
-    protected $timeout = 3;
-    protected $expireAt = 60; //in second
+    protected $timeout = 1; //in seconds
+    protected $expireAt = 60; //in seconds
     protected $redis = true;
 
     public function callService($url, $method = null, $data = [])
     {
         if (empty($url)) {
-            return response()->json([
-                'success'   => false,
-                'message'   => 'Define endpoint of service'
-            ]);
+            return responisme()->makeError('Endpoint of service must be define');
         }
 
-        if ($this->redis) {
+        try {
+            $this->isHealthyService($url);
+            
             $key = $url.($method ?? 'GET').json_encode($data);
-            $cache = Redis::get($key);
 
-            if (!empty($cache)) {
-                return json_decode($cache);
+            if ($this->redis) {                
+                $cache = Redis::get($key);
+    
+                if (!empty($cache)) {
+                    return json_decode($cache);
+                }
             }
-        }
 
+            $this->sendRequest($url, $method, $data, $key);
+        } catch (\Throwable $th) {
+           $this->isHealthyService($th, false);
+
+           throw (new StarterKitException(transMessageException($th)));
+        }
+                        
+    }
+
+    private function sendRequest($url, $method, $data, $key)
+    {
         $http = Http::acceptJson()
             ->retry(1,1)
             ->timeout($this->timeout)
             ->withHeaders(['Accept' => 'application/json', 'Authorization' => request()->header('authorization')]);
 
         $method = strtolower($method ?? 'get');
+
         $response = $http->$method($url, array_merge($data , ['auth_user_kong' => request('auth_user_kong')]))->json();                        
         
         if ($this->redis) {
             $this->setRedis($key, $response);
         }
 
-        return $response;        
+        return $response;
+    }
+
+    private function isHealthyService($url, $status = true)
+    {
+        $parse = parse_url($url);
+
+        $sessionKey = ($parse['host'] ?? $url).($parse['port'] ?? '');
+
+        if ($status) {
+            // if session exists mean service is down.
+            if (session()->has($sessionKey)){
+                return responisme()->makeError(session($sessionKey));
+            } 
+        }else{
+            session()->flash($sessionKey, $url->getMessage());
+        }
+                   
     }
 
     public function timeout($second = 3)
